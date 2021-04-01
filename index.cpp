74fe1dfb0baf6deae906b2af26d6b070a520cfd3
reyesdiego90 <jreyesz@miumg.edu.gt>
2021-03-31 22:27:52 -0600

    Se creo una funcion para los tokens

--- index.cpp
+++ index.cpp
@@ -22,30 +22,38 @@ string reservada[17] = {"IF", "THEN", "ELSE","BEGIN", "END",
 string operadoBinario[4] = {"-", "+", "*", "/"};
 string operadorLogico[6] = {"<", ">", "==", "<=", "=>", "!="};
 
+void token(int, string[], string);
+
 
 int main (){
     int j = 0;
     string linea;
     archivo.open("lectura.txt");
-    if(archivo.is_open()){
-        while(!archivo.eof()){
+    if(archivo.is_open()) {
+        while(!archivo.eof()) {
             getline(archivo, linea);
             v2.push_back(linea);
         }
+
     }else{
         cout<<"El archivo no se encontrado"<<endl;
     }
-    cout<<"Palabras Reservadas: "<<endl;
-    for(size_t i = 0; i<v2.size(); i++){
-        
-        for(j=0; j<17; j++){
-            const char *letra = v2[i].c_str();
-            const char *busqueda = reservada[j].c_str();
+    
+    token(14, reservada, "Palabra Reservada: ");
+    token(4, operadoBinario, "Binarios: ");
+    token(6, operadorLogico, "Operadores Logicos: ");
+
+}
+
+void token(int num, string palabras[], string oracion){ 
+    for(size_t i = 0; i<v2.size(); i++) {
+        const char *letra = v2[i].c_str();
+        for(int j=0; j<num; j++){
+            const char *busqueda = palabras[j].c_str();
             const char *resultado = strstr(letra, busqueda);
-            if(strstr(v2[i].c_str(), reservada[j].c_str())){
-                cout<<i<<") "<<reservada[j]<<endl;
+            if(resultado){
+                cout<<oracion<<palabras[j]<<endl;
             }
         }
     }
-   
 }
